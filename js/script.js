a3ca7d287ad2c68f918555e31659785b04eb6b04
Craka250 <otienojobochieng19@gmail.com>
2026-03-25 22:07:13 -0700

    Updated the homepage UI

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// TASK COUNT (for dashboard preview)
 let tasks = [];
 
 function updateTaskCount(){
   let count = document.getElementById('taskCount');
   if(count){
-    count.innerText = tasks.length;
+    count.innerText = tasks.length + ' Tasks Pending';
   }
 }
 
